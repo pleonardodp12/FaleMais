2bd822f2e69e48eb6b8ba536660eed88304e262f
pleonardodp12 <pauloleonardo_dp@outlook.com>
2020-07-22 16:52:10 -0300

    ligacao classe

---
+++ backend/src/Ligacao.js
@@ -0,0 +1,10 @@
+class Ligacao {
+  constructor(origem, destino, duracao, plano) {
+    this.origem = origem;
+    this.destino = destino;
+    this.duracao = duracao;
+    this.plano = plano;
+  }
+}
+
+module.exports = Ligacao;
